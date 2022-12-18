ecdf8ebd96736f9c9a541d26c60ba4f73b5c3b35
Nicolas Baldassarri <MY_NAME@example.comnico_9_98@hotmail.it>
2022-12-18 23:38:30 +0100

    add update

--- sys/main.lua
+++ sys/main.lua
@@ -4,10 +4,10 @@ local main = basalt.createFrame()
 local button = main --> Basalt returns an instance of the object on most methods, to make use of "call-chaining"
         :addButton() --> This is an example of call chaining
         :setPosition(4,4) 
-        :setText("FORMATTA IL PC")
+        :setText("AGGIORNA")
         :onClick(
             function() 
-                shell.run("sys/format")
+                shell.run("sys/update")
             end)
 
 basalt.autoUpdate()
---
+++ sys/update.lua
@@ -0,0 +1,7 @@
+shell.run("rm /sys")
+shell.run("rm /startup")
+
+ 
+term.clear()
+term.setCursorPos(1,1)
+shell.run("pastebin run 1fRWcR0m")
