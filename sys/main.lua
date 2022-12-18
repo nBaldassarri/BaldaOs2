35ddaf80b23ea8024af01fe46dbddff3b35a83cf
Nicolas Baldassarri <MY_NAME@example.comnico_9_98@hotmail.it>
2022-12-18 23:35:50 +0100

    add gui format

--- sys/main.lua
+++ sys/main.lua
@@ -1,15 +1,13 @@
 local basalt = require("basalt")
 
-i = 0;
 local main = basalt.createFrame()
 local button = main --> Basalt returns an instance of the object on most methods, to make use of "call-chaining"
         :addButton() --> This is an example of call chaining
         :setPosition(4,4) 
-        :setText("Click me!")
+        :setText("FORMATTA IL PC")
         :onClick(
             function() 
-                i = i + 1
-                basalt.debug("I got clicked " + i + " times") 
+                shell.run("sys/format")
             end)
 
 basalt.autoUpdate()
