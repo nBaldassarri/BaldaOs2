814043f2c0ff6ce0f69cca0791b7c50263bb8114
Nicolas Baldassarri <MY_NAME@example.comnico_9_98@hotmail.it>
2022-12-18 23:51:50 +0100

    fix

--- pages/home/main.lua
+++ pages/home/main.lua
@@ -4,7 +4,7 @@ basalt.setVariable("buttonClick", function()
     shell.run("sys/update")
 end)
   
-local main = basalt.createFrame():addLayout("example.xml")
+local main = basalt.createFrame():addLayout("template.xml")
   
 
 basalt.autoUpdate()
