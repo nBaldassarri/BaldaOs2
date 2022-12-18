598bb3a320d4d64a43d7e1176331bbe59700b260
Nicolas Baldassarri <MY_NAME@example.comnico_9_98@hotmail.it>
2022-12-18 23:49:24 +0100

    text xml

---
+++ pages/home/main.lua
@@ -0,0 +1,10 @@
+local basalt = require("basalt")
+
+basalt.setVariable("buttonClick", function()
+    shell.run("sys/update")
+end)
+  
+local main = basalt.createFrame():addLayout("example.xml")
+  
+
+basalt.autoUpdate()
