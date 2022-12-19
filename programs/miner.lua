1f68ebc6c87a1fd78c00b13b8befed3db2d4c29f
Nicolas Baldassarri <MY_NAME@example.comnico_9_98@hotmail.it>
2022-12-19 22:00:14 +0100

    test

--- programs/miner.lua
+++ programs/miner.lua
@@ -1,6 +1,21 @@
-turtle.refuel(0)
+function eat()
+    print("eating...")
+    for i = 1, 16 do
+        turtle.select(i) 
+        if turtle.refuel(0)
+            local stack = math.ceil(turtle.getItemCount(i))
+            turtle.refuel(stack)
+            print('Fuel-Level: ' .. turtle.getFuelLevel());
+            return
+        end
+    end
+    print('Fuel not found')
+end
+
+eat()
+
 i = 0
-while i<10 do 
+while i < 10 do 
     turtle.digDown()
     turtle.down()
     i = i+1
