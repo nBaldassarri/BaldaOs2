a9b7c6b8b783dae60afaf18d7e4663f5f3e6cf62
Edoardo <edoardo2846@gmail.com>
2022-12-20 23:34:40 +0100

    gdfg

--- programs/enderedorisp.lua
+++ programs/enderedorisp.lua
@@ -6,6 +6,7 @@ if statoChannel == 1 then
   repeat
     event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
     print(("Messaggio Ricevuto questa è la risposta che ha scritto edo"):format(side, channel, replyChannel, distance, tostring(message)))
+  until channel == 1
   end
 elseif statoChannel == 0 then
   print("DIO CANE NON VA")
