4708fa1a7d54f8e2daa648154f1728b40edd814e
Edoardo <edoardo2846@gmail.com>
2022-12-20 21:12:21 +0100

    test

--- programs/edo.lua
+++ programs/edo.lua
@@ -1,21 +1,11 @@
 local dfpwm = require("cc.audio.dfpwm")
+local speaker = peripheral.find("speaker")
 
-local encoder = dfpwm.make_encoder()
 local decoder = dfpwm.make_decoder()
+for chunk in io.lines("programs/sounds/requestedproduction.dfpwm", 16 * 1024) do
+    local buffer = decoder(chunk)
 
-local out = fs.open("requestedproduction.dfpwm", "wb")
-for input in io.lines("programs/sounds/requestedproduction.dfpwm", 16 * 1024 * 2) do
-  local decoded = decoder(input)
-  local output = {}
-
-  -- Read two samples at once and take the average.
-  for i = 1, #decoded, 2 do
-    local value_1, value_2 = decoded[i], decoded[i + 1]
-    output[(i + 1) / 2] = (value_1 + value_2) / 2
-  end
-
-  out.write(encoder(output))
-
-  sleep(0) -- This program takes a while to run, so we need to make sure we yield.
+    while not speaker.playAudio(buffer) do
+        os.pullEvent("speaker_audio_empty")
+    end
 end
-out.close()
