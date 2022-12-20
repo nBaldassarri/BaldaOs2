3bd30fde07b02f86e9e88bc1f56a503c47b971a9
Edoardo <edoardo2846@gmail.com>
2022-12-20 23:02:04 +0100

    dfg

--- programs/enderedo.lua
+++ programs/enderedo.lua
@@ -1,9 +1,20 @@
-local modem = peripheral.find("modem") or error("No modem attached", 0)
 local dfpwm = require("cc.audio.dfpwm")
 local speaker = peripheral.find("speaker")
 local decoder = dfpwm.make_decoder()
 
 for chunk in io.lines("programs/sounds/requestedproduction.dfpwm", 16 * 1024 * 2) do
+    local buffer = decoder(chunk)
+
+    while not speaker.playAudio(buffer) do
+        os.pullEvent("speaker_audio_empty")
+    end
+end
+
+
+
+local modem = peripheral.find("modem") or error("No modem attached", 0)
+
+for chunk in io.lines("programs/sounds/finischedproduction.dfpwm", 16 * 1024 * 2) do
     local buffer = decoder(chunk)
 
     while not speaker.playAudio(buffer) do
