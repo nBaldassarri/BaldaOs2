dff767124d227a30acfc5e9ad8827bebe46a3cc3
Edoardo <edoardo2846@gmail.com>
2022-12-20 23:06:15 +0100

    dsadsa

--- programs/enderedo.lua
+++ programs/enderedo.lua
@@ -1,14 +1,15 @@
 local dfpwm = require("cc.audio.dfpwm")
 local speaker = peripheral.find("speaker")
-local decoder = dfpwm.make_decoder()
 
-for chunk in io.lines("programs/sounds/requestedproduction.dfpwm", 16 * 1024) do
+local decoder = dfpwm.make_decoder()
+for chunk in io.lines("programs/sounds/requestedproduction.dfpwm", 16 * 1024 * 2) do
     local buffer = decoder(chunk)
 
     while not speaker.playAudio(buffer) do
         os.pullEvent("speaker_audio_empty")
     end
 end
+
 
 
 
