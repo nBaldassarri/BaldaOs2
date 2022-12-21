ad5b13cb781449aa81122aa90f3e5cdfeaa0425b
Edoardo <edoardo2846@gmail.com>
2022-12-21 21:56:39 +0100

    asd

--- programs/enderedorisp.lua
+++ programs/enderedorisp.lua
@@ -1,12 +1,12 @@
 /*statoChannel = modem.isOpen(1)	
 if statoChannel == 1 then*/
-  while true do 
+while true do 
     local event, side, channel, replyChannel, message, distance
     repeat
       event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
       print(("Messaggio Ricevuto questa è la risposta che ha scritto edo"):format(side, channel, replyChannel, distance, tostring(message)))
     until channel == 1
-  end
+ end
 /*elseif statoChannel == 0 then
   print("DIO CANE NON VA")
 end
