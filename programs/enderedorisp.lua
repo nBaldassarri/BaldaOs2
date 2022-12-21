635a6f75ad79fff253af0acbdfa7899c30238774
Edoardo <edoardo2846@gmail.com>
2022-12-21 22:40:44 +0100

    asdsa

--- programs/enderedorisp.lua
+++ programs/enderedorisp.lua
@@ -1,9 +1,8 @@
-while true do 
     local event, side, channel, replyChannel, message, distance
     repeat
       event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
       print(("Messaggio Ricevuto questa è la risposta che ha scritto edo"):format(side, channel, replyChannel, distance, tostring(message)))
     until channel == 1
- end
+
 
 
