fc80a768413849c9310615790cd8a0a44c995201
Edoardo <edoardo2846@gmail.com>
2022-12-21 21:57:56 +0100

    dssad

--- programs/enderedorisp.lua
+++ programs/enderedorisp.lua
@@ -1,5 +1,3 @@
-/*statoChannel = modem.isOpen(1)	
-if statoChannel == 1 then*/
 while true do 
     local event, side, channel, replyChannel, message, distance
     repeat
@@ -7,7 +5,5 @@ while true do
       print(("Messaggio Ricevuto questa è la risposta che ha scritto edo"):format(side, channel, replyChannel, distance, tostring(message)))
     until channel == 1
  end
-/*elseif statoChannel == 0 then
-  print("DIO CANE NON VA")
-end
+
 
