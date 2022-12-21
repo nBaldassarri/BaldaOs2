66929dab454c8a0e517ed28abe8942330a82b1ba
Edoardo <edoardo2846@gmail.com>
2022-12-21 21:55:34 +0100

    dsa

--- programs/enderedorisp.lua
+++ programs/enderedorisp.lua
@@ -7,7 +7,7 @@ if statoChannel == 1 then*/
       print(("Messaggio Ricevuto questa è la risposta che ha scritto edo"):format(side, channel, replyChannel, distance, tostring(message)))
     until channel == 1
   end
-//*elseif statoChannel == 0 then
+/*elseif statoChannel == 0 then
   print("DIO CANE NON VA")
 end
 
