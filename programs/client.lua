6a3b183e78563b4be3c80136509121db0da2ec46
Nicolas Baldassarri <MY_NAME@example.comnico_9_98@hotmail.it>
2022-12-22 20:58:04 +0100

    test

--- programs/client.lua
+++ programs/client.lua
@@ -1,2 +1,11 @@
 local modem = peripheral.find("modem") or error("No modem attached", 0)
+
+if modem.isOpen(DEFAULT_CHANNEL) == true then
+  modem.close(DEFAULT_CHANNEL)
+else
+  modem.open(DEFAULT_CHANNEL)
+  print("CANALE " .. DEFAULT_CHANNEL .." APERTO")
+end
+
+
 modem.transmit(10, 20, "Hello, world!")
