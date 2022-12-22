5447d8096c823f7245938bbfa7e8bf699d1762c6
Nicolas Baldassarri <MY_NAME@example.comnico_9_98@hotmail.it>
2022-12-22 20:53:53 +0100

    test

--- programs/server.lua
+++ programs/server.lua
@@ -8,6 +8,8 @@ else
     print("CANALE " .. DEFAULT_CHANNEL .." APERTO")
 end
 
+modem.transmit(10, 20, "Hello, world!")
+
 -- And wait for a reply
 local event, side, channel, replyChannel, message, distance
 repeat
