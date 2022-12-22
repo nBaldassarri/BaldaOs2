abd7306430d83e1b65f31b7122d9e6ab5d5c5e3d
Edoardo <edoardo2846@gmail.com>
2022-12-22 21:25:10 +0100

    fdssdf

--- programs/enderedo.lua
+++ programs/enderedo.lua
@@ -41,9 +41,9 @@ else
     end
 end
 i = 1
-while true do
+
     local event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
     print(("Message received on side %s on channel %d (reply to %d) from %f blocks away with message %s"):format(side, channel, replyChannel, distance, tostring(message)))
     i = i + 1
     modem.transmit(1, 1, "Risposta n" .. tostring(int))
-  end
+
