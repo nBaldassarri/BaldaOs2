9fc28c8e386efed39e04c5ae324a9d62698e3012
Edoardo <edoardo2846@gmail.com>
2022-12-22 20:57:39 +0100

    dsa

--- programs/enderedorisp.lua
+++ programs/enderedorisp.lua
@@ -26,7 +26,7 @@ end
 if modem.isOpen(1) == true then
   modem.transmit(1,1,"PRIMO MESSAGGIO INVIATO")
   i = 1
-  rednet.send(1,1,"QUESTO E IL MESSAGGIO DI PARTENZA")
+  modem.transmit(1,1,"QUESTO E IL MESSAGGIO DI PARTENZA")
   while true do
     local event, channel, replyChannel, message = os.pullEvent("modem_message")
     print(("MESSAGGIO RICEVUTO %s on channel %d (reply to %d) from %f blocks away with message %s"):format(side, channel, replyChannel, distance, tostring(message)))
