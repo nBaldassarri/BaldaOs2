6f5bfb9cea41af2435f029461b5437d1339e38b7
Edoardo <edoardo2846@gmail.com>
2022-12-22 21:26:38 +0100

    dsa

--- programs/enderedorisp.lua
+++ programs/enderedorisp.lua
@@ -27,12 +27,12 @@ if modem.isOpen(1) == true then
   modem.transmit(1, 1, "PRIMO MESSAGGIO INVIATO")
   i = 1
   modem.transmit(1 ,1, "QUESTO E IL MESSAGGIO DI PARTENZA")
-
+  while true do
     local event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
     print(("MESSAGGIO RICEVUTO %s on channel %d (reply to %d) from %f blocks away with message %s"):format(side, channel, replyChannel, distance, tostring(message)))
     i = i + 1
     modem.transmit(1, 1, "messaggio n" .. tostring(int))
-
+  end
 else
   print("NON RIESCO A FARE UN CAZZO")
 end
