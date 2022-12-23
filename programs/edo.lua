64a22e3ec281cbb0707035214ecf0246e92e21f2
Edoardo <edoardo2846@gmail.com>
2022-12-23 22:33:07 +0100

    dsad

--- programs/edo.lua
+++ programs/edo.lua
@@ -1,11 +1,28 @@
-local dfpwm = require("cc.audio.dfpwm")
-local speaker = peripheral.find("speaker")
+local modem = peripheral.find("modem") or error("No modem attached", 0)
 
-local decoder = dfpwm.make_decoder()
-for chunk in io.lines("programs/sounds/requestedproduction.dfpwm", 16 * 1024 * 2) do
-    local buffer = decoder(chunk)
-
-    while not speaker.playAudio(buffer) do
-        os.pullEvent("speaker_audio_empty")
-    end
+if modem.isWireless == true then
+    print("IL MODEM E WIRELESS")
+else
+    print("IL MODEM NON e WIRELESS")
 end
+
+i = 1
+while i <= 50 do
+    local channel = i
+    if modem.isOpen(channel) == true then 
+        modem.close(channel)
+    end
+    modem.open(i)
+    print("CHANNEL" .. i .. "OPENED")
+    i = i + 1
+end
+
+while true do
+    local event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
+    
+
+    print(("Message received on side %s on channel %d (reply to %d) from %f blocks away with message %s"):format(side, channel, replyChannel, distance, tostring(message)))
+    i = i + 1
+    modem.transmit(1, 1, "Risposta n" .. tostring(i))
+  end
+  
