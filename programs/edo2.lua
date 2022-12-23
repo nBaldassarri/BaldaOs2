158ff72fa1473c96a9f3abde00327dc3ae94ec8a
Edoardo <edoardo2846@gmail.com>
2022-12-23 23:15:55 +0100

    asd

--- programs/edo2.lua
+++ programs/edo2.lua
@@ -1,3 +1,8 @@
 local modem = peripheral.find("modem") or error("No modem attached", 0)
 
 modem.transmit(45, 45 , "suono")
+
+while true do
+    local event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
+    print(("Message received on side %s on channel %d (reply to %d) from %f blocks away with message %s"):format(side, channel, replyChannel, distance, tostring(message)))
+  end
