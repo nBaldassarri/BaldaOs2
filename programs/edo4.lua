c7de69e5c10d991e5a5723117b87eeeccaef3a9e
Edoardo <edoardo2846@gmail.com>
2022-12-24 23:57:39 +0100

    asd

--- programs/edo4.lua
+++ programs/edo4.lua
@@ -1,13 +1,16 @@
 i = 0
 for i = 1, 16 do
     turtle.select(i) 
-    local data = turtle.getItemDetail()
-    if data ~= nil then
-        local item = data.name
-        if item == "minecraft:diamond_pickaxe" then
-            turtle.equipLeft()
+    local data = turtle.getItemDetail()ù
+
+    if data then
+        print("Item name: ", data.name)
+        print("Item damage value: ", data.damage)
+        print("Item count: ", data.count)
+        if data.name == "minecraft:diamond_pickaxe"
+        turtle.equipLeft()
         end
     end
-return
+    return
 end
 
