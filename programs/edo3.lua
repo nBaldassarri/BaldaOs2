780670954864324091e9786350948ebc0538dab1
Edoardo <edoardo2846@gmail.com>
2022-12-24 17:11:42 +0100

    asd

--- programs/edo3.lua
+++ programs/edo3.lua
@@ -48,7 +48,7 @@ function squareMiningEmpty(perimeter)
     refuelling()
 
     for i = 1, 16 do
-        if turtle.getItemDetail(i) == "minecraft:diamond_pickaxe"   
+        if turtle.getItemDetail(i) == "minecraft:diamond_pickaxe" then
             turtle.equipLeft()
             return
         end
