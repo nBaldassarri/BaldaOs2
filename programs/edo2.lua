a5fe97bbf4c426ef2a3c53f8223c5b626fa351f1
Edoardo <edoardo2846@gmail.com>
2022-12-24 13:12:57 +0100

    dasd

--- programs/edo2.lua
+++ programs/edo2.lua
@@ -5,4 +5,4 @@ modem.transmit(45, 45 , "suono")
 while true do
     local event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
     print(("Message received on side %s on channel %d (reply to %d) from %f blocks away with message %s"):format(side, channel, replyChannel, distance, tostring(message)))
-  end
+end
