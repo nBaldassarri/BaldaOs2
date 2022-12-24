b856b7fccfda4342ab310a36e4fb465760066cc7
Edoardo <edoardo2846@gmail.com>
2022-12-24 23:51:35 +0100

    asd

--- programs/edo4.lua
+++ programs/edo4.lua
@@ -2,7 +2,8 @@ i = 0
 for i = 1, 16 do
     turtle.select(i) 
     local data = turtle.getItemDetail()
-    if data.name == "minecraft:diamond_pickaxe" then
+    local item = data.name
+    if item == "minecraft:diamond_pickaxe" then
         turtle.equipLeft()
     end
 return
