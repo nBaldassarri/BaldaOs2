7513c4ac361cdf9b05169ed4f4aac50c44f73f4f
Nicolas Baldassarri <MY_NAME@example.comnico_9_98@hotmail.it>
2022-12-25 22:19:20 +0100

    test

--- sys/libraries/utils.lua
+++ sys/libraries/utils.lua
@@ -1,29 +1,31 @@
-refuelling = function()
-    print("sono entrato in refuel")
-    print("REFUELLING")
-    for i = 1, 16 do
-        turtle.select(i) 
-        if turtle.refuel(0) then 
-            turtle.refuel(turtle.getItemCount(i))
-            return
-        end
-    end
-    print('Fuel not found')
-end
-
-pickaxeResearch = function()
-    local i = 0
-    for i = 1, 16 do
-        turtle.select(i) 
-        local data = turtle.getItemDetail()
-        if data then
-            print("Item name: ", data.name)
-            print("Item damage value: ", data.damage)
-            print("Item count: ", data.count)
-            if data.name == "minecraft:diamond_pickaxe" then
-                turtle.equipLeft()
+return {
+    refuelling = function()
+        print("sono entrato in refuel")
+        print("REFUELLING")
+        for i = 1, 16 do
+            turtle.select(i) 
+            if turtle.refuel(0) then 
+                turtle.refuel(turtle.getItemCount(i))
                 return
             end
         end
+        print('Fuel not found')
+    end,
+
+    pickaxeResearch = function()
+        local i = 0
+        for i = 1, 16 do
+            turtle.select(i) 
+            local data = turtle.getItemDetail()
+            if data then
+                print("Item name: ", data.name)
+                print("Item damage value: ", data.damage)
+                print("Item count: ", data.count)
+                if data.name == "minecraft:diamond_pickaxe" then
+                    turtle.equipLeft()
+                    return
+                end
+            end
+        end
     end
-end
+}
